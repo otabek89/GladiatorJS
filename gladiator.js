c0a32b9acee56b5758fc196aad5835fbdb55ada1
otabek89 <44710488+otabek89@users.noreply.github.com>
2018-11-27 08:55:02 -0800

    Delete gladiator.js

--- gladiator.js
+++
@@ -1,9 +0,0 @@
-class Gladiator
-    constructor(name, weapon) {
-        this.name = name;
-        this.weapon = weapon;
-    }
-    if (Gladiator.weapon !== ("Spear" || "Club" || "Trident"); {
-        alert("This ain't that kinda fight.")
-    }
-}
